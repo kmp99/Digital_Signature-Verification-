9afc10820986136ab4e665ef40f0691665fe9c9b
DESKTOP-QVMHNLF\Xtrans <krishan@xtranssolutions.com>
2019-08-25 18:07:49 +0530

    ABC

---
+++ contracts/Verifier.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.21 <0.6.0;
+contract Verifier {
+    function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public returns (address) {
+        return ecrecover(msgHash, v, r, s);
+    }
+
+    function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public returns (bool) {
+        return ecrecover(msgHash, v, r, s) == _addr;
+    }
+}
